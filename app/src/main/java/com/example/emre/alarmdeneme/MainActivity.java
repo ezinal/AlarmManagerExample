ee4903b9da7affb2186ab136dd8bff8ab038ee49
ezinal <emrezinal151@gmail.com>
2018-08-22 23:06:38 +0300

    first commit

---
+++ app/src/main/java/com/example/emre/alarmdeneme/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.emre.alarmdeneme;
+
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button start;
+    Button stop;
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
+    Button btnDatePicker, btnTimePicker;
+    TextView txtTime,alarmInfo;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private int hour,min;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start= findViewById(R.id.button);
+        alarmInfo = findViewById(R.id.alarmSetInfo);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAlert();
+                alarmInfo.setText("Alarm is: Set");
+            }
+        });
+
+        stop =findViewById(R.id.stop);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (alarmManager!= null) {
+                    alarmManager.cancel(pendingIntent);
+                }
+                alarmInfo.setText("Alarm is: Stopped");
+            }
+        });
+
+        btnTimePicker=(Button)findViewById(R.id.btn_time);
+        txtTime= findViewById(R.id.in_time);
+
+        btnTimePicker.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+//        if (v == btnDatePicker) {
+//
+//            // Get Current Date
+//            final Calendar c = Calendar.getInstance();
+//            mYear = c.get(Calendar.YEAR);
+//            mMonth = c.get(Calendar.MONTH);
+//            mDay = c.get(Calendar.DAY_OF_MONTH);
+//
+//
+//            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+//                    new DatePickerDialog.OnDateSetListener() {
+//
+//                        @Override
+//                        public void onDateSet(DatePicker view, int year,
+//                                              int monthOfYear, int dayOfMonth) {
+//
+//                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+//
+//                        }
+//                    }, mYear, mMonth, mDay);
+//            datePickerDialog.show();
+//        }
+        if (v == btnTimePicker) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+                            String s = hourOfDay + ":" + minute;
+                            txtTime.setText(s);
+                            hour = hourOfDay;
+                            min = minute;
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
+    }
+
+
+    public void startAlert(){
+        Intent intent = new Intent(this, MyBroadcastReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 234324243, intent, 0);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        // Set the alarm to start at 8:30 a.m.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, min);
+
+        // setRepeating() lets you specify a precise custom interval--in this case,
+        //  once a day.
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                1000 * 60 * 60 * 24, pendingIntent);
+        Toast.makeText(this, "Alarm set",Toast.LENGTH_LONG).show();
+    }
+}
